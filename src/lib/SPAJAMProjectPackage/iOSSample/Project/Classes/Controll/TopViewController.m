58983b2e28f913a5134e4420328cf5c069e6458c
inukai <inukai1@ouka.local>
2015-05-31 08:55:47 +0900

    マイページのセル

--- src/lib/SPAJAMProjectPackage/iOSSample/Project/Classes/Controll/TopViewController.m
+++ src/lib/SPAJAMProjectPackage/iOSSample/Project/Classes/Controll/TopViewController.m
@@ -157,7 +157,7 @@
                     // 自分の所属ファミリアが取れたので、続いてActivity一覧を取得する
                 }else{
                     isGod = NO;
-                    myPageView = [[MyPageView alloc] initWithFrame:CGRectMake(0, 0, self.view.width,480) WithTopViewController:self :isGod];
+                    myPageView = [[MyPageView alloc] initWithFrame:CGRectMake(0, 0, self.view.width,460) WithTopViewController:self :isGod];
                     [dataListView setTableHeaderView:myPageView];
                     // 自分の所属ファミリアが取れたので、続いてActivity一覧を取得する
                 }
@@ -235,7 +235,7 @@
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (0 < activityData.total) {
-        return 50;
+        return 60;
     }
     return 0;
 }
--- src/lib/SPAJAMProjectPackage/iOSSample/Project/Classes/View/ActivityCellView.m
+++ src/lib/SPAJAMProjectPackage/iOSSample/Project/Classes/View/ActivityCellView.m
@@ -19,28 +19,65 @@
 {
     self = [super initWithFrame:argFrame];
     if (self) {
-        // レコード名
-        UILabel *nameLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, self.width - 20, 15)];
-        nameLabel.text = argActivityModel.log;
-        nameLabel.textColor = [UIColor grayColor];
-        nameLabel.font = [UIFont fontWithName:@"HiraKakuProN-W3" size:13];
-        nameLabel.backgroundColor = [UIColor clearColor];
-        nameLabel.textAlignment = NSTextAlignmentLeft;
-        [self addSubview:nameLabel];
-
-        // 日時
-        UILabel *dateLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.width - 110, nameLabel.y + nameLabel.height + 5, 100, 10)];
-        dateLabel.text = argActivityModel.modified;
-        dateLabel.textColor = [UIColor lightGrayColor];
-        dateLabel.font = [UIFont fontWithName:@"HiraKakuProN-W3" size:9];
-        dateLabel.backgroundColor = [UIColor clearColor];
-        dateLabel.textAlignment = NSTextAlignmentRight;
-        [self addSubview:dateLabel];
         
-        // セパレータ
-        UIView *separator = [[UIView alloc] initWithFrame:CGRectMake(5, self.height -5, self.width - 10, 1)];
-        separator.backgroundColor = [UIColor lightGrayColor];
-        [self addSubview:separator];
+//        UIImageView *bgImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"bg_button"]];
+//        bgImageView.x = 0;
+//        bgImageView.y = 0;
+//        [self addSubview:bgImageView];
+        
+        // セパレータ用画像
+        UIImageView *separatorImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"devider"]];
+        separatorImageView.x = 0;
+        separatorImageView.y = 0;
+        [self addSubview:separatorImageView];
+        
+        // 階段マーク
+        UIImageView *kaidanImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"icon_kaidan"]];
+        kaidanImageView.x = 30;
+        kaidanImageView.y = 25;
+        [self addSubview:kaidanImageView];
+        
+        // Activity
+        UILabel *logLabel = [[UILabel alloc] initWithFrame:CGRectMake(55, 25, 175, 15)];
+        logLabel.text = argActivityModel.log;
+        logLabel.textColor =  RGBA(230, 197, 107, 1);
+        logLabel.font = [UIFont fontWithName:@"HiraKakuProN-W3" size:16];
+        logLabel.backgroundColor = [UIColor clearColor];
+        //logLabel.backgroundColor = [UIColor redColor];
+        logLabel.textAlignment = NSTextAlignmentLeft;
+        [self addSubview:logLabel];
+        
+        // 作業時間
+        UILabel *timeLabel = [[UILabel alloc] initWithFrame:CGRectMake(logLabel.x+logLabel.width+5, 25, 25, 15)];
+        timeLabel.text = [NSString stringWithFormat:@"%@h",argActivityModel.time];
+        timeLabel.textColor =  RGBA(230, 197, 107, 1);
+        timeLabel.font = [UIFont fontWithName:@"HiraKakuProN-W3" size:16];
+        timeLabel.backgroundColor = [UIColor clearColor];
+        //timeLabel.backgroundColor = [UIColor blueColor];
+        timeLabel.textAlignment = NSTextAlignmentRight;
+        [self addSubview:timeLabel];
+
+        // 星
+        NSString *starFileName;
+        if([@"0" isEqual:argActivityModel.good]){
+            starFileName = @"icon_star_gray";
+        }else{
+            starFileName = @"icon_star";
+        }
+        UIImageView *starImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:starFileName]];
+        starImageView.x = timeLabel.x+timeLabel.width+7;
+        starImageView.y = timeLabel.y;
+        [self addSubview:starImageView];
+        
+        // 作成日時
+        UILabel *createLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.width*0.5-7, logLabel.y+logLabel.height+2, self.width*0.5, 15)];
+        createLabel.text = argActivityModel.created;
+        createLabel.textColor =  RGBA(230, 197, 107, 1);
+        createLabel.font = [UIFont fontWithName:@"HiraKakuProN-W3" size:10];
+        createLabel.backgroundColor = [UIColor clearColor];
+        createLabel.textAlignment = NSTextAlignmentLeft;
+        [self addSubview:createLabel];
+        
     }
     return self;
 }
