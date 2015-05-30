78edd72cbbfc09f3111218e95c840ad9cbbd2a13
cy-takashi-takeuchi <takashi_takeuchi@cybird.co.jp>
2015-05-30 19:49:47 +0900

    viewコントローラー

---
+++ src/lib/SPAJAMProjectPackage/iOSSample/Project/Classes/Controll/FamiliarListViewController.h
@@ -0,0 +1,18 @@
+//
+//  FamiliarListViewController.h
+//
+//  Created by saimushi on 2014/09/19.
+//  Copyright (c) 2014年 saimushi. All rights reserved.
+//
+
+#import "common.h"
+#import "EGORefreshTableHeaderView.h"
+
+@interface FamiliarListViewController : UIViewControllerBase <UITableViewDelegate, UITableViewDataSource, EGORefreshTableHeaderDelegate, UIScrollViewDelegate>
+{
+    // Protected
+}
+
+// Public
+
+@end
---
+++ src/lib/SPAJAMProjectPackage/iOSSample/Project/Classes/Controll/FamiliarRegisterViewController.h
@@ -0,0 +1,18 @@
+//
+//  FamiliarRegisterViewController.h
+//
+//  Created by saimushi on 2014/09/19.
+//  Copyright (c) 2014年 saimushi. All rights reserved.
+//
+
+#import "common.h"
+#import "EGORefreshTableHeaderView.h"
+
+@interface FamiliarRegisterViewController : UIViewControllerBase <UITableViewDelegate, UITableViewDataSource, EGORefreshTableHeaderDelegate, UIScrollViewDelegate>
+{
+    // Protected
+}
+
+// Public
+
+@end
